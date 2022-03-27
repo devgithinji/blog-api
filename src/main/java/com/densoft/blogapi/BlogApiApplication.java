f1ba6c801caeea180a1c2a94c39974ceffdac90e
dennis <wakahiad@gmail.com>
2022-03-27 21:38:17 +0300

    deployment setup

--- src/main/java/com/densoft/blogapi/BlogApiApplication.java
+++ src/main/java/com/densoft/blogapi/BlogApiApplication.java
@@ -1,12 +1,16 @@
 package com.densoft.blogapi;
 
+import com.densoft.blogapi.entity.Role;
+import com.densoft.blogapi.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BlogApiApplication {
+public class BlogApiApplication implements CommandLineRunner {
 
     @Bean
     public ModelMapper modelMapper() {
@@ -17,4 +21,17 @@ public class BlogApiApplication {
         SpringApplication.run(BlogApiApplication.class, args);
     }
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+    }
 }
